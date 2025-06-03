7dad1be61a6e52d5f424669dfc25325e3bac25fb
v-suyarov <90338171+v-suyarov@users.noreply.github.com>
2025-06-03 21:15:32 +0300

    clear personal data

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-_api_id = 13669173
-_api_hash = '489ab59d0bea0b75b33bb3f566fe7dfa'
-_phone = '+79381674707'
+# https://my.telegram.org/auth
+_api_id = 10000000
+_api_hash = '489...'
+_phone = '+7938...'
 _session_name = 'test.session'
-bot_token = '7792649344:AAGZj4_XAVH9tAQk4T7JakQhybueYo7wpHc'
+bot_token = '77...'
